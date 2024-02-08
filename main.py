e656640672d924be3fda3aa58341eafe8ac79863
Jazper26 <jazperhaapin89Gmail.com>
2024-02-08 14:01:56 +0800

    Adding an html file that see the number location exactly on the map

--- main.py
+++ main.py
@@ -23,3 +23,7 @@ results = geocoder.geocode(query)
 lat = results[0]['geometry']['lat']
 lng = results[0]['geometry']['lng']
 print (lat, lng)
+
+map_location = folium.Map(location = [lat, lng], zoom_start=9)
+folium.Marker([lat, lng], popup = number_location).add_to(map_location)
+map_location.save("mylocation.html")
