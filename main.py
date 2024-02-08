aedda68271924f6279b4459605796521b9e00692
Jazper26 <jazperhaapin89Gmail.com>
2024-02-08 14:10:57 +0800

    Instead of putting the number in source code. I put a scanner that you can input your number

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import phonenumbers
 from phonenumbers import geocoder
-from test import number
+#from test import number
 import folium
 
 Keys = "e61087ec5aab40af84ad23daacb57d97"
 
+number = input("Enter phone number with country code (+63 if ph): ")
 check_number = phonenumbers.parse(number)
 number_location = geocoder.description_for_number(check_number, "en")
 print(number_location)
