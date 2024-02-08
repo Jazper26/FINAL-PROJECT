488452012af5b5b8a2c5d7be7e56c6f4a01497e2
Jazper26 <jazperhaapin89Gmail.com>
2024-02-08 13:57:42 +0800

    Apply keys to geocoder and add a function that tracks the latitude and longitude of the number

--- main.py
+++ main.py
@@ -15,3 +15,10 @@ service_provider = phonenumbers.parse(number)
 print (carrier.name_for_number(service_provider, "en"))
 
 from opencage.geocoder import OpenCageGeocode
+geocoder = OpenCageGeocode(Keys)
+
+query = str(number_location)
+results = geocoder.geocode(query)
+
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
