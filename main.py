b03ce3c9e43157bfcb3a8079c5ae35f71ea09e69
Jazper26 <jazperhaapin89Gmail.com>
2024-02-08 13:42:37 +0800

    Add a function that can tell what type of sim the number have

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ import folium
 check_number = phonenumbers.parse(number)
 number_location = geocoder.description_for_number(check_number, "en")
 print(number_location)
+
+
+from phonenumbers import carrier
+service_provider = phonenumbers.parse(number)
+print (carrier.name_for_number(service_provider, "en"))
